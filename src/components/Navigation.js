510f3fee02d36b41b1fa2f4cfe50a6dd350b6a7d
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-09-03 01:07:43 -0400

    add link items to navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import {Navbar, Nav} from 'react-bootstrap'
+import {Container, Navbar, Nav} from 'react-bootstrap'
 
 const Navigation = (props) => {
   return (
     <Navbar bg='dark' variant='dark'>
       <Container>
-      <Navbar.Brand href='#home'>Genuinfluence</Navbar.Brand>
+      <Navbar.Brand href='#home'>GENUINFLUENCE</Navbar.Brand>
+      <Nav className='me-auto'>
+        <Nav.Link href='#home'>HOME</Nav.Link>
+        <Nav.Link href='#login'>LOGIN</Nav.Link>
+        <Nav.Link href='#join'>JOIN</Nav.Link>
+        <Nav.Link href='#about'>ABOUT</Nav.Link>
+        <Nav.Link href='#talent'>VIEW TALENT</Nav.Link>
+      </Nav>
       </Container>
     </Navbar>
   )
