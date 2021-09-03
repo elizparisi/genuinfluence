ec0a5041f1ad324d9a9af033fcbc3d3420070c4b
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-09-03 00:21:18 -0400

    update navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Navbar, Nav} from 'react-bootstrap'
 
 const Navigation = (props) => {
   return (
-
+    <Navbar bg='dark' variant='dark'>
+      <Container>
+      <Navbar.Brand href='#home'>Genuinfluence</Navbar.Brand>
+      </Container>
+    </Navbar>
   )
 }
 
