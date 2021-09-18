499fc2cdbddd7970fd964db38c90ad2807ba55b7
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-09-17 23:28:04 -0400

    build form + add fields for email + password

--- src/components/SigninForm.js
+++ src/components/SigninForm.js
@@ -6,7 +6,22 @@ class SigninForm extends React.Component {
   render() {
     return (
       <div>
+      <Container>
+        <Form>
+          <Form.Group>
+          <Form.Label>Email: </Form.Label>
+          <Form.Control type="email" name="email"></Form.Control>
+          </Form.Group>
 
+          <Form.Group>
+          <Form.Label>Password: </Form.Label>
+          <Form.Control type="password" name="password"></Form.Control>
+          </Form.Group>
+
+          <Button type="submit">Sign In</Button>
+
+        </Form>
+      </Container>
       </div>
     )
   }
