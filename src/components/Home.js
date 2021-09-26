dad7bcc16e2fecffd1ddbf8d011994226e056bbe
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-09-26 16:40:07 -0400

    add header, description + buttons to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Button} from 'react-bootstrap'
 
 const Home = () => {
   return (
     <div className='homepage'>
-
+      <h1>GENUINFLUENCE</h1>
+      <p>Welcome to Geninfluence. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac tortor dignissim convallis aenean et. Pharetra sit amet aliquam id diam maecenas ultricies mi eget.</p>
+      <Button>Genuinfluencer</Button>    <Button>Client</Button>
     </div>
   )
 }
