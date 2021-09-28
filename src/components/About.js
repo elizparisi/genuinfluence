8e36c6700de252881aff25184a9cf640035cf55e
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-09-27 20:41:46 -0400

    starter code for about component + some test text

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  <div className="about">
+    <Container>
+      <h2>services</h2>
+      <Row>
+        <Col>
+          <p>Test text. Test text. Test text.</p>
+        </Col>
+      </Row>
+    </Container>
+  </div>
+}
+
+export default About
