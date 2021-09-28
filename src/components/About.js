a2accfbf1c66219852af425b5867b6e4c2660827
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-09-27 21:04:43 -0400

    import container, row, col + fix return syntax

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
 
 const About = () => {
+
+  return (
   <div className="about">
     <Container>
       <h2>services</h2>
@@ -11,6 +14,7 @@ const About = () => {
       </Row>
     </Container>
   </div>
+  )
 }
 
 export default About
