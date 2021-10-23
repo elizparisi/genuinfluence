3d795f2747c262f705a71ce8b17138efe73b2a94
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-10-23 18:24:46 -0400

    add class to homepage h1 + change font

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import {Button} from 'react-bootstrap'
 const Home = () => {
   return (
     <div className='homepage'>
-      <h1>GENUINFLUENCE</h1>
+      <h1 className="home-h1">GENUINFLUENCE</h1>
       <p>Welcome to Geninfluence. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac tortor dignissim convallis aenean et. Pharetra sit amet aliquam id diam maecenas ultricies mi eget.</p>
       <button className="creators-button">creators</button>    <button className="users-button">users</button>
     </div>
