e97c0647fecc3e5c4fa11f1d3fb3851a9e2ecedb
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-10-25 23:35:04 -0400

    add class + placeholders to signUpForm

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -30,20 +30,20 @@ class SignupForm extends React.Component {
     return (
       <div>
         <Container>
-          <Form>
+          <Form className="signup-form">
             <Form.Group>
             <Form.Label>Username: </Form.Label>
-            <Form.Control type="text" name="username"></Form.Control>
+            <Form.Control type="text" name="username" placeholder="username"></Form.Control>
             </Form.Group>
 
             <Form.Group>
             <Form.Label>Email: </Form.Label>
-            <Form.Control type="email" name="email"></Form.Control>
+            <Form.Control type="email" name="email" placeholder="email"></Form.Control>
             </Form.Group>
 
             <Form.Group>
             <Form.Label>Password: </Form.Label>
-            <Form.Control type="password" name="password"></Form.Control>
+            <Form.Control type="password" name="password" placeholder="password"></Form.Control>
             </Form.Group>
 
             <Button type="submit">Join</Button>
