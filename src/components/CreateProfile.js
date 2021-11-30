b1c1957e913baa76b44b3790149ef3a3599ca644
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-11-29 23:45:13 -0500

    add onSubmit to profile form + make it controlled form

--- src/components/CreateProfile.js
+++ src/components/CreateProfile.js
@@ -40,46 +40,46 @@ class CreateProfile extends React.Component {
     return (
       <div>
         <Container>
-          <Form className="profile-creator">
+          <Form className="profile-creator" onSubmit={this.handleSubmit}>
             <h1>Create Profile</h1>
             <Form.Group>
             <Form.Label>Profile Image: </Form.Label>
-            <Form.Control type="file" placeholder="profileimage" />
+            <Form.Control type="file" placeholder="profileimage" value={this.state.profileimage} />
             </Form.Group>
 
             <Form.Group>
             <Form.Label>Full Name: </Form.Label>
-            <Form.Control type="text" name="fullname" placeholder="fullname"></Form.Control>
+            <Form.Control type="text" name="fullname" placeholder="fullname" value={this.state.fullname}></Form.Control>
             </Form.Group>
 
             <Form.Group>
             <Form.Label>Bio: </Form.Label>
-            <Form.Control type="textarea" name="bio" placeholder="bio"></Form.Control>
+            <Form.Control type="textarea" name="bio" placeholder="bio" value={this.state.bio}></Form.Control>
             </Form.Group>
 
             <Form.Group>
             <Form.Label>Location: </Form.Label>
-            <Form.Control type="text" name="location" placeholder="location"></Form.Control>
+            <Form.Control type="text" name="location" placeholder="location" value={this.state.location}></Form.Control>
             </Form.Group>
 
             <Form.Group>
             <Form.Label>Age: </Form.Label>
-            <Form.Control type="number" name="age" placeholder="age"></Form.Control>
+            <Form.Control type="number" name="age" placeholder="age" value={this.state.age}></Form.Control>
             </Form.Group>
 
             <Form.Group>
             <Form.Label>Category: </Form.Label>
-            <Form.Control type="text" name="category" placeholder="select category"></Form.Control>
+            <Form.Control type="text" name="category" placeholder="select category" value={this.state.category}></Form.Control>
             </Form.Group>
 
             <Form.Group>
             <Form.Label>Social Media: </Form.Label>
-            <Form.Control type="text" name="socialmedia" placeholder="social media channel"></Form.Control>
+            <Form.Control type="text" name="socialmedia" placeholder="social media channel" value={this.state.socialmedia}></Form.Control>
             </Form.Group>
 
             <Form.Group>
             <Form.Label>Followers: </Form.Label>
-            <Form.Control type="number" name="followers" placeholder="followers"></Form.Control>
+            <Form.Control type="number" name="followers" placeholder="followers" value={this.state.followers}></Form.Control>
             </Form.Group>
 
             <br></br>
